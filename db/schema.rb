d074975c30a85921b7e20fddc77e0fad949bcc71
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-03-11 00:51:13 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190311004857) do
+ActiveRecord::Schema.define(version: 20190311005101) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20190311004857) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.boolean  "post_status"
   end
 
 end
