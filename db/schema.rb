a6052a0fd80d9c8c65ac5aa7f856d30a33d407f6
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-03-11 00:53:55 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20190311005101) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.boolean  "post_status"
+    t.string  "post_status"
   end
 
 end
